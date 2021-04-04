1deea0be913a704e7e657a75db5c9fda30e12ed9
TarCV <2857607+TarCV@users.noreply.github.com>
2021-04-04 03:11:17 +0300

    Update license

--- README.md
+++ README.md
@@ -39,10 +39,11 @@ with managing and setting up servers.
 ##Copyright
 
 Copyright 2013 Best. Ever.
+
 Copyright 2021 TarCV
 
-Licensed under the GNU General Public License, Version 2.0 (the "License"); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
+Licensed under the GNU Affero General Public License, Version 3 (the "License"); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
 
-http://www.gnu.org/licenses/gpl-2.0.html
+https://www.gnu.org/licenses/agpl-3.0.en.html
 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied. See the License for the specific language governing permissions and limitations under the License.
--- src/main/java/org/bestever/bebot/Bot.java
+++ src/main/java/org/bestever/bebot/Bot.java
@@ -2,15 +2,17 @@
 // Copyright (C) 2012-2013 Best-Ever & The Sentinel's Playground
 // Copyright (C) 2021 TarCV
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3
+// as published by the Free Software Foundation.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 // --------------------------------------------------------------------------
 
