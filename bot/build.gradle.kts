7aec6bee8b8fe564d1f7159882677b7c61ab04be
TarCV <2857607+TarCV@users.noreply.github.com>
2021-04-12 03:09:47 +0300

    Move bot code to a submodule

---
+++ bot/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    application
+}
+
+application {
+    mainClass.set("org.bestever.bebot.Bot")
+}
+
+dependencies {
+    implementation("com.mewna:catnip:3.0.0")
+    implementation("mysql:mysql-connector-java:5.0.8")
+    implementation("org.ini4j:ini4j:0.5.2")
+    implementation("javax.json:javax.json-api:1.1.4")
+    implementation("org.glassfish:javax.json:1.1.4")
+    implementation("com.google.guava:guava:30.1-jre")
+
+    testImplementation("junit:junit:4.12")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,19 @@
 plugins {
     application
+    id("org.sonarqube").version("3.1.1")
 }
 
-repositories {
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+subprojects {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
 }
 
-dependencies {
-    implementation("com.mewna:catnip:3.0.0")
-    implementation("mysql:mysql-connector-java:5.0.8")
-    implementation("org.ini4j:ini4j:0.5.2")
-    implementation("javax.json:javax.json-api:1.1.4")
-    implementation("org.glassfish:javax.json:1.1.4")
-    implementation("com.google.guava:guava:30.1-jre")
-
-    testImplementation("junit:junit:4.12")
-}
-
-application {
-    mainClass.set("org.bestever.bebot.Bot")
+sonarqube {
+    properties {
+        property("sonar.projectKey", "TarCV_ZBot")
+        property("sonar.organization", "tarcv")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
